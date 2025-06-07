da5110f7934ac804ee0c8a93f9109b4ea2908abe
Asma Hamid <asmahamid1382@gmail.com>
2025-06-07 14:22:38 +0330

    fb_cellinfo/modelchange

--- polaris_server/cellinfo/models.py
+++ polaris_server/cellinfo/models.py
@@ -1,45 +1,60 @@
 from django.db import models
 
+TECHNOLOGY_CHOICES = [
+    ('GSM', 'GSM'),
+    ('GPRS', 'GPRS'),
+    ('EDGE', 'EDGE'),
+    ('UMTS', 'UMTS'),
+    ('HSPA', 'HSPA'),
+    ('HSPA+', 'HSPA+'),
+    ('LTE', 'LTE'),
+    ('LTE-Adv', 'LTE Advanced'),
+    ('5G', '5G'),
+]
+
 class BaseSignalTest(models.Model):
     client_id = models.CharField(max_length=100)
     timestamp = models.DateTimeField()
-    generation = models.CharField(max_length=3, choices=[
-        ('2G', '2G'),
-        ('3G', '3G'),
-        ('4G', '4G'),
-        ('5G', '5G'),
-    ])
+    technology = models.CharField(max_length=20, choices=TECHNOLOGY_CHOICES)
     latitude = models.FloatField()
     longitude = models.FloatField()
     plmn_id = models.CharField(max_length=10)
     cell_id = models.BigIntegerField()
-    
+    arfcn = models.IntegerField(null=True, blank=True)
+    band = models.IntegerField(null=True, blank=True)
+
+    @property
+    def generation(self):
+        if self.technology in ['GSM', 'GPRS', 'EDGE']:
+            return '2G'
+        elif self.technology in ['UMTS', 'HSPA', 'HSPA+']:
+            return '3G'
+        elif self.technology in ['LTE', 'LTE-Adv']:
+            return '4G'
+        elif self.technology == '5G':
+            return '5G'
+        return 'Unknown'
+
+
     class Meta:
         abstract = True
 
 class SignalTest2G(BaseSignalTest):
-    rxlev = models.FloatField()
     lac = models.IntegerField()
-    arfcn = models.IntegerField(null=True, blank=True)
-    band = models.IntegerField(null=True, blank=True)
+    rac = models.IntegerField(null=True, blank=True)
+    rxlev = models.FloatField(null=True, blank=True)
 
 class SignalTest3G(BaseSignalTest):
-    rscp = models.FloatField()
-    ecn0 = models.FloatField()
     lac = models.IntegerField()
-    arfcn = models.IntegerField(null=True, blank=True)
-    band = models.IntegerField(null=True, blank=True)
+    rscp = models.FloatField()
+    ecn0 = models.FloatField(null=True, blank=True)
 
 class SignalTest4G(BaseSignalTest):
-    rsrp = models.FloatField()
-    rsrq = models.FloatField()
     tac = models.IntegerField()
-    arfcn = models.IntegerField(null=True, blank=True)
-    band = models.IntegerField(null=True, blank=True)
+    rsrp = models.FloatField()
+    rsrq = models.FloatField(null=True, blank=True)
 
 class SignalTest5G(BaseSignalTest):
-    rsrp = models.FloatField()
-    rsrq = models.FloatField()
     tac = models.IntegerField()
-    arfcn = models.IntegerField(null=True, blank=True)
-    band = models.IntegerField(null=True, blank=True)
+    rsrp = models.FloatField()
+    rsrq = models.FloatField(null=True, blank=True)
