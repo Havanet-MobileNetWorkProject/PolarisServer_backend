fa6bc678aab63c124db2743d05d928409bd24b5b
Asma Hamid <asmahamid1382@gmail.com>
2025-08-22 22:51:52 +0330

    final commit

--- polaris_server/polaris_server/settings.py
+++ polaris_server/polaris_server/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'tests',
     'threshold',
     'authentication',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -98,23 +100,23 @@ WSGI_APPLICATION = 'polaris_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'polaris_server',  
-        'USER': 'havanet',        
-        'PASSWORD': '2J*f.*(E_BxRF3rP',    
-        'HOST': 'localhost',           
-        'PORT': '3306',                 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'polaris_server',  
+#         'USER': 'havanet',        
+#         'PASSWORD': '2J*f.*(E_BxRF3rP',    
+#         'HOST': 'localhost',           
+#         'PORT': '3306',                 
+#     }
+# }
 
 
 # Password validation
