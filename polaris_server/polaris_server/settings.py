ebcee3d57da53d9d3420001a2798c6fec13962e0
Asma Hamid <asmahamid1382@gmail.com>
2025-08-22 13:28:09 +0330

    database

--- polaris_server/polaris_server/settings.py
+++ polaris_server/polaris_server/settings.py
@@ -98,10 +98,21 @@ WSGI_APPLICATION = 'polaris_server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'polaris_server',  
+        'USER': 'havanet',        
+        'PASSWORD': '2J*f.*(E_BxRF3rP',    
+        'HOST': 'localhost',           
+        'PORT': '3306',                 
     }
 }
 
